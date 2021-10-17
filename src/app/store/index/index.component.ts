08cba01377624e0c9b76bf386d135514d7cf187d
milad <frontend.crew@parnasweb.com>
2021-10-17 13:03:37 +0330

    ui index v3

--- src/app/store/index/index.component.ts
+++ src/app/store/index/index.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { SwiperModule } from "swiper/angular";
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import {Component, OnInit} from '@angular/core';
+import {SwiperModule} from "swiper/angular";
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+declare let $: any;
+
 @Component({
   selector: 'prs-index',
   templateUrl: './index.component.html',
@@ -9,9 +12,30 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    $(document).ready(() => {
+      $(".collapse-1").click(() => {
+        $(".icon-1").toggleClass("icon-rotate-collapse")
+      })
+      $(".collapse-2").click(() => {
+        $(".icon-2").toggleClass("icon-rotate-collapse")
+      })
+      $(".collapse-3").click(() => {
+        $(".icon-3").toggleClass("icon-rotate-collapse")
+      })
+      $(".collapse-4").click(() => {
+        $(".icon-4").toggleClass("icon-rotate-collapse")
+      })
+      $(".collapse-5").click(() => {
+        $(".icon-5").toggleClass("icon-rotate-collapse")
+      })
+      $(".collapse-6").click(() => {
+        $(".icon-6").toggleClass("icon-rotate-collapse")
+      })
+    })
 
   }
 
