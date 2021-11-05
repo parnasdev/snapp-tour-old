103110b5c99781e95e47ae636a5410020b878e6f
iman <iman.hp.dev5294@gmail.com>
2021-11-05 10:27:46 +0330

    add tour fixed

--- src/app/tour/panel/add/add.component.ts
+++ src/app/tour/panel/add/add.component.ts
@@ -399,6 +399,13 @@ export class AddComponent implements OnInit {
     })
   }
 
-
+   clean(obj:any):void {
+    for (var propName in obj) {
+      if (obj[propName] === null || obj[propName] === undefined) {
+        delete obj[propName];
+      }
+    }
+    return obj
+  }
 }
 
