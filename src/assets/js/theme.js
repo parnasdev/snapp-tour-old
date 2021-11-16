c92429a0e64d8972b03cc88a9cacc9672e2310d0
milad <frontend.crew@parnasweb.com>
2021-11-16 16:07:54 +0330

    thumbnail-tour-two ui desktop & mobile

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -4,4 +4,21 @@ $(document).ready(function (){
     $(".menu-box-mobi").slideToggle(500)
     $(".icon-menu").toggleClass("icon-menu-active")
   })
+
+
+
+
+
+
+
+})
+
+
+$(window).scroll(function (){
+  if($(this).scrollTop() > 900){
+    $(".title-section-two").addClass("title-fixed")
+  }else {
+    $(".title-section-two").removeClass("title-fixed")
+  }
+  // -----------------
 })
