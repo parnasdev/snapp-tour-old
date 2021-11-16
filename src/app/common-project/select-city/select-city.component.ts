e7a4b2b22910a68b52c0cb61f620a40dd4e972e7
iman <iman.hp.dev5294@gmail.com>
2021-11-16 13:31:57 +0330

    city select added

--- src/app/common-project/select-city/select-city.component.ts
+++ src/app/common-project/select-city/select-city.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
+import {MessageService} from "../../Core/Services/message.service";
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from "rxjs/operators";
+import {CityListRequestDTO, CityResponseDTO} from "../../Core/Models/cityDTO";
+import {CityApiService} from "../../Core/Https/city-api.service";
 
 @Component({
   selector: 'prs-select-city',
@@ -6,10 +12,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-city.component.scss']
 })
 export class SelectCityComponent implements OnInit {
+  @Output() citySelected = new EventEmitter()
+  @Input() cities: CityResponseDTO[] = []
 
-  constructor() { }
+  @Input() callCity: boolean = false;
+  @Input() inCommingCity: any
 
-  ngOnInit(): void {
+  constructor(
+    public cityApi: CityApiService,
+    public message: MessageService) {
+  }
+
+  cityFC = new FormControl();
+  filteredOptions!: Observable<CityResponseDTO[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.cityFC.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(value: string): CityResponseDTO[] {
+    const filterValue = value.toLowerCase();
+
+    return this.cities.filter(city => city.name.toLowerCase().includes(filterValue));
+  }
+
+  changed(item: any): void {
+    this.citySelected.emit(item)
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.inCommingCity) {
+      this.cityFC.setValue(this.inCommingCity.name)
+    }
+
+    if (this.callCity) {
+      this.getCities()
+    }
+  }
+
+  getCities(): void {
+    const req: CityListRequestDTO = {
+      type: null,
+      hasHotel: true,
+      hasTour: false,
+      search: null,
+      perPage: 20
+    }
+    this.cityApi.getCities(req).subscribe((res: any) => {
+      if (res.isDone) {
+        this.cities = res.data;
+      }
+    }, (error: any) => {
+      this.message.error()
+    })
   }
 
 }
