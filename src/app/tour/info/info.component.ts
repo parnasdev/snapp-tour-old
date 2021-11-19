4889131f2be105d2b6f052c937f9b46d514d1c13
m.mohamadi <m.mohamadi490@gmail.cm>
2021-11-19 14:15:22 +0330

    add print packages to tour info

--- src/app/tour/info/info.component.ts
+++ src/app/tour/info/info.component.ts
@@ -40,10 +40,11 @@ export class InfoComponent implements OnInit {
               public checkErrorService: CheckErrorService,
               public errorService: ErrorsService,
               public responsiveService: ResponsiveService,
-  public message: MessageService) {
+              public message: MessageService) {
     this.isMobile = responsiveService.isMobile();
     this.isTablet = responsiveService.isTablet();
   }
+
   setThumbsSwiper(swiper: any): void {
     this.thumbsSwiper = swiper;
   }
@@ -55,7 +56,7 @@ export class InfoComponent implements OnInit {
     window.scrollTo(0, 0)
   }
 
-  getTourInfo(): void{
+  getTourInfo(): void {
     this.loading = true;
     this.tourApiService.getTour(this.tourSlug).subscribe((res: any) => {
       if (res.isDone) {
@@ -71,9 +72,46 @@ export class InfoComponent implements OnInit {
     });
   }
 
-
-  getStarterPrice(): number{
+  getStarterPrice(): number {
     return this.tourInfo.defineTour ? this.tourInfo.packages[0].prices.single : this.tourInfo.packages[0].prices.twin;
+  }
+
+  getTagsHtml(tagName: keyof HTMLElementTagNameMap): string {
+    const htmlStr: string[] = [];
+    const elements = document.getElementsByTagName(tagName);
+    for (let idx = 0; idx < elements.length; idx++) {
+      htmlStr.push(elements[idx].outerHTML);
+    }
+
+    return htmlStr.join('\r\n');
+  }
+
+  print() {
+    let contents, popupWin;
+    // @ts-ignore
+    contents = document.getElementById('output').innerHTML;
+    const stylesHtml = this.getTagsHtml('style');
+    const linksHtml = this.getTagsHtml('link');
+    const scriptsHtml = this.getTagsHtml('script');
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    // @ts-ignore
+    popupWin.document.open();
+    // @ts-ignore
+    popupWin.document.write(`
+      <html dir="rtl">
+        <head>
+            ${linksHtml}
+            ${stylesHtml}
+          <title>پکیج ها</title>
+        </head>
+        <body onload="window.print()">
+            ${contents}
+            ${scriptsHtml}
+        </body>
+      </html>`
+    );
+    // @ts-ignore
+    popupWin.document.close();
   }
 
 }
