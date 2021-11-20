838a352405c20c64ec237f3aa5b1ae8227774384
milad <frontend.crew@parnasweb.com>
2021-11-20 14:14:09 +0330

    city info

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,19 +1,23 @@
-$(document).ready(function (){
+$(document).ready(function () {
   // mobile-menu
-  $(".icon-menu").click(function (){
+  $(".icon-menu").click(function () {
     $(".menu-box-mobi").slideToggle(500)
     $(".icon-menu").toggleClass("icon-menu-active")
   })
+// ------
+  $(".button-down-scroll").click(function () {
+    $(".scrolling-area").animate({scrollTop: 400}, 300)
+  })
 
-$(".button-down-scroll").click(function (){
-  $(".scrolling-area").animate({scrollTop: 400} , 300)
 })
-})
-$(window).scroll(function (){
-  if($(this).scrollTop() > 900){
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 900) {
     $(".title-section-two").addClass("title-fixed")
-  }else {
+  } else {
     $(".title-section-two").removeClass("title-fixed")
   }
   // -----------------
 })
+$(function () {
+  $("#tab-info-city").tabs();
+});
