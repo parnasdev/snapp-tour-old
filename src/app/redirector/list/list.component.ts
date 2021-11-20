48f3a2f800e8c4f42ddbed3f5eb4584a5b6c3a61
iman <iman.hp.dev5294@gmail.com>
2021-11-20 14:07:09 +0330

    redirector delete

--- src/app/redirector/list/list.component.ts
+++ src/app/redirector/list/list.component.ts
@@ -3,6 +3,7 @@ import {MessageService} from "../../Core/Services/message.service";
 import {RedirectorApiService, RedirectorListResDTO} from "../../Core/Https/redirector-api.service";
 import {MatDialog} from "@angular/material/dialog";
 import {SetComponent} from "../set/set.component";
+import {AlertDialogComponent, AlertDialogDTO} from "../../common-project/alert-dialog/alert-dialog.component";
 
 @Component({
   selector: 'prs-list',
@@ -40,6 +41,34 @@ export class ListComponent implements OnInit {
         this.getList()
       }
     })
+  }
+
+  delete(id: any): void {
+    this.api.deleteRedirector(id).subscribe((res: any) => {
+      if (res.isDone) {
+        this.getList()
+      }
+      this.messageService.custom(res.message);
+    }, (error: any) => {
+
+    })
+  }
+
+  deleteClicked(id:any) {
+    const obj: AlertDialogDTO = {
+      description: 'حذف شود؟',
+      icon: 'null',
+      title: 'اطمینان دارید'
+    };
+    const dialog = this.dialog.open(AlertDialogComponent, {
+      width: '30%',
+      data: obj
+    });
+    dialog.afterClosed().subscribe(result => {
+      if (result) {
+        this.delete(id);
+      }
+    });
   }
 
   edit(item: any): void {
