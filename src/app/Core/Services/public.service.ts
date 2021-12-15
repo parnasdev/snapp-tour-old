77029463917a9d45325d9ba00e6690334b526720
m.mohamadi <m.mohamadi490@gmail.cm>
2021-12-15 12:34:55 +0330

    add set prefix func in public service

--- src/app/Core/Services/public.service.ts
+++ src/app/Core/Services/public.service.ts
@@ -2,43 +2,47 @@ import {Injectable} from '@angular/core';
 import {HttpHeaders} from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PublicService {
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    getDefaultHeaders(): any {
-        return {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            })
-        };
-    }
+  getDefaultHeaders(): any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      })
+    };
+  }
 
-    substringText(count: number, str: string): string {
-        if (str) {
-            if (str.length > count) {
-                return str.substring(0, count) + '...';
-            } else {
-                return str;
-            }
-        } else {
-            return '';
-        }
-    }
-
-    fixNumbers(str: any): string {
-        const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
-        const EnglishNumbers = [/0/g, /1/g, /2/g, /3/g, /4/g, /5/g, /6/g, /7/g, /8/g, /9/g];
-        if (typeof str === 'string') {
-            for (let i = 0; i <= str.length; i++) {
-                str = str.replace(persianNumbers[i], i).replace(EnglishNumbers[i], i);
-            }
-        }
+  substringText(count: number, str: string): string {
+    if (str) {
+      if (str.length > count) {
+        return str.substring(0, count) + '...';
+      } else {
         return str;
+      }
+    } else {
+      return '';
     }
+  }
+
+  fixNumbers(str: any): string {
+    const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
+    const EnglishNumbers = [/0/g, /1/g, /2/g, /3/g, /4/g, /5/g, /6/g, /7/g, /8/g, /9/g];
+    if (typeof str === 'string') {
+      for (let i = 0; i <= str.length; i++) {
+        str = str.replace(persianNumbers[i], i).replace(EnglishNumbers[i], i);
+      }
+    }
+    return str;
+  }
+
+  setPrefix(image: string): string {
+    return 'https://hamnavaz.com/panel/' + image;
+  }
 
 }
