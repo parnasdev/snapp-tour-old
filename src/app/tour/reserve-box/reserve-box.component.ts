2adc210a8daea25186f709589561d492afd9cc66
m.mohamadi <m.mohamadi490@gmail.cm>
2022-01-16 13:05:03 +0330

    some change in logs and list for reserve

--- src/app/tour/reserve-box/reserve-box.component.ts
+++ src/app/tour/reserve-box/reserve-box.component.ts
@@ -4,6 +4,7 @@ import {MessageService} from "../../Core/Services/message.service";
 import {TourApiService} from "../../Core/Https/tour-api.service";
 import {ReserveReqDTO} from "../../Core/Models/tourDTO";
 import {FormControl} from "@angular/forms";
+import {CityResponseDTO} from "../../Core/Models/cityDTO";
 
 @Component({
   selector: 'prs-reserve-box',
@@ -15,7 +16,17 @@ export class ReserveBoxComponent implements OnInit {
   req!: ReserveReqDTO
   countFC = new FormControl('1');
   phoneFC = new FormControl();
-  @Input() city = '';
+  @Input() city: CityResponseDTO = {
+    description: '',
+    id: 0,
+    images: [],
+    slugEn: '',
+    name: '',
+    faq: [],
+    nameEn: '',
+    slug: '',
+    type: 1,
+  };
   @Input() month = '';
 
   constructor(public message: MessageService,
@@ -31,7 +42,7 @@ export class ReserveBoxComponent implements OnInit {
       package_id: null,
       noPackage: true,
       phone: this.phoneFC.value,
-      city_id: this.city,
+      city_id: this.city.id.toString(),
       month: this.month
     }
     this.api.reserve(this.req).subscribe((res: any) => {
