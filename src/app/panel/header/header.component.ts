ff6b2a3622125b7455c13bc0065010a62f7017df
milad <frontend.crew@parnasweb.com>
2022-01-17 12:46:18 +0330

    panel edit front

--- src/app/panel/header/header.component.ts
+++ src/app/panel/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SessionService} from "../../Core/Services/session.service";
 import {AuthApiService} from "../../Core/Https/auth-api.service";
 import {MessageService} from "../../Core/Services/message.service";
 import {CheckErrorService} from "../../Core/Services/check-error.service";
 import {Router} from "@angular/router";
-
+declare let $: any;
 @Component({
   selector: 'prs-header',
   templateUrl: './header.component.html',
@@ -12,7 +12,6 @@ import {Router} from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
   isLoading = false;
-
   constructor(public session: SessionService,
               public api: AuthApiService,
               public router: Router,
@@ -21,7 +20,9 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
   }
+
   logOut(): void {
     this.isLoading = true
     this.api.logout().subscribe((res: any) => {
--- src/app/panel/panel/panel.component.ts
+++ src/app/panel/panel/panel.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import {SettingService} from "../../Core/Services/setting.service";
 
+declare let $: any;
+
 @Component({
   selector: 'prs-panel',
   templateUrl: './panel.component.html',
@@ -15,9 +17,13 @@ export class PanelComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    $('.btn-sidebar').click(() => {
+      $('.sidebar').animate({
+        width: ['toggle', 'swing']
+      })
+      $('.main-page').toggleClass('main-full').animate(1500)
+    })
     this.title.setTitle('پنل کاربری همنواز')
-
   }
 
 }
