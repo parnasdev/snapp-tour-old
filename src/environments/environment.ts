2b2f5be48b59a3ce339723836d73640a97eb9f60
m.mohammadi <m.mohamadi490@gmail.com>
2023-03-20 00:44:33 +0330

    change api and app name to sandbox

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   BACK_END_IP: 'https://snapptour-api.iran.liara.run/api/v1/',
+  BACK_END_IP_v2: 'https://snapptour-sandbox.iran.liara.run/api/v2/',
   BACK_END_UPLOAD: 'https://snapptour-api.iran.liara.run/api/',
   PANEL_URL: 'agency.bilitja.com',
   SITE_URL: 'tour.snapptrip.com',
