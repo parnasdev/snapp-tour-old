b230a9ed9ef585d6637fa5db67f53696c22f6d11
m.mohammadi <m.mohamadi490@gmail.com>
2023-03-23 19:54:05 +0430

    change api address in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   BACK_END_IP: 'https://snapptour-api.iran.liara.run/api/v1/',
-  BACK_END_IP_v2: 'https://snapptour-sandbox.iran.liara.run/api/v2/',
+  BACK_END_IP_v2: 'https://snapptour-api.iran.liara.run/api/v2/',
   fileManagerAddress: '',
   BACK_END_UPLOAD: 'https://snapptour-api.iran.liara.run/api/',
   PANEL_URL: 'agency.bilitja.com',
   SITE_URL: 'tour.snapptrip.com',
   // LOCAL_URL: 'snapptour.iran.liara.run',
-  LOCAL_URL: 'snapp-sandbox.iran.liara.run',
+  LOCAL_URL: 'snapptour.iran.liara.run',
   // LOCAL_URL: 'localhost',
   VERSION: '1.1.2'
 };
